5ad4875716b47aae0787f71305eca7746361e50e
Vitaly Matveev <vitaly.matveev@gmail.com>
2023-05-17 19:27:09 +0300

    code correction

--- transcribe.py
+++ transcribe.py
@@ -3,7 +3,16 @@ from google.cloud import speech_v1p1beta1 as speech
 import os
 import audioread
 
-# ... Your other code ...
+# Set up Google Cloud credentials (replace 'path_to_service_account_key.json' with your own key file)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "loyal-surfer-333701-b1b753aa1fde.json"
+
+# Create a client for the Google Cloud Speech-to-Text API
+client = speech.SpeechClient()
+
+# Function to convert audio file to MP3 format
+def convert_to_mp3(input_file, output_file):
+    audio = AudioSegment.from_file(input_file)
+    audio.export(output_file, format="mp3")
 
 # Function to transcribe speech from audio file
 def transcribe_speech(audio_file, accent):
@@ -47,3 +56,35 @@ def transcribe_speech(audio_file, accent):
         # Extract and return transcriptions
         transcriptions = [result.alternatives[0].transcript for result in response.results]
         return "\n".join(transcriptions)
+
+# Streamlit app
+def main():
+    st.title("Speech-to-Text Transcription")
+    st.write("Upload an audio file to transcribe.")
+
+    # File upload
+    audio_file = st.file_uploader("Upload Audio File", type=["m4a"])
+
+    # Accent selection
+    accent = st.text_input("Accent (Optional)")
+
+    # Transcription button
+    if st.button("Transcribe"):
+        if audio_file is not None:
+            # Save the uploaded file temporarily
+            temp_file = os.path.join(".", audio_file.name)
+            with open(temp_file, "wb") as file:
+                file.write(audio_file.getbuffer())
+            # Perform transcription
+            transcription = transcribe_speech(temp_file, accent)
+            # Display the transcription
+            st.success("Transcription:")
+            st.write(transcription)
+            # Delete the temporary files
+            os.remove(temp_file)
+            os.remove(mp3_file)
+        else:
+            st.error("Please upload an audio file.")
+
+if __name__ == "__main__":
+    main()
