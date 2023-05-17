effd720551309e047906a873862b9fdea2f79037
Vitaly Matveev <vitaly.matveev@gmail.com>
2023-05-17 14:25:48 +0300

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -23,7 +23,7 @@ def transcribe_speech(audio_file, accent):
             language_code="en-IN",
             use_enhanced=True,
             model="video",
-            metadata={"interactionType": "DISCUSSION", "industryNaicsCodeOfAudio": 54},
+            metadata={"industryNaicsCodeOfAudio": 54},
             diarization_config=speech.SpeakerDiarizationConfig(
                 enable_speaker_diarization=True,
                 min_speaker_count=2,
